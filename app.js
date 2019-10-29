042de71574ce3c8353d0708d5fc647d3f5119eb1
Icey Isaak <iceyisaak@gmail.com>
2019-10-29 14:05:25 +0100

    minor correction

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View Engine Setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static Folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/send', (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Family Name: ${req.body.familyName}</li>
+        <li>Company: ${req.body.company}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Phone Number: ${req.body.phoneNumber}</li>
+    </ul>
+    <p>Subject</p>
+    <p>${req.body.subject}</p>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com", // Your smtp
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'YOUR@EMAIL.COM', // your email address
+            pass: 'YOUR_PASSWORD' // your password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact" <iceyisaak@gmail.com>', // sender address
+        to: "iceyisaak@gmail.com", // list of receivers
+        subject: "Nodemailer Contact Request", // Subject line
+        text: "Hello world?", // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+        res.render('contact', { msg: 'The email has been sent' });
+
+    });
+
+});
+
+app.listen(3000, () => console.log('Server Started...'));
